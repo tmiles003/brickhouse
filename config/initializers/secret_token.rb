46e3bc9874706176803ac5c320cebe40f71be76b
Chris Kleeschulte <laconia@gmail.com>
2012-02-23 21:06:36 -0500

    first commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brickhouse::Application.config.secret_token = '8a1948eeb32e92e245376dd7df48858834d7e950d8d1bb44f8c30dc4c89c6e21a96a132e9e19287d76466368beafdfc7251869062e11fb671d62e1ea4ea7a0e5'
